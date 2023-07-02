f3406c90613a75752655812f8e30f58f5277984e
GrungeElFz <elf0114_@hotmail.com>
2023-07-02 13:31:09 -0500

    Modified:   Import `Threlte`, `Three`, and `Utils` (src/routes/+page.svelte)
                Added `Canvas`
                  ↳ `PerspectiveCamera`
                    ↳ `OrbitControls`
                  ↳ `AmbientLight`
                  ↳ `Mesh`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+	import * as Threlte from '@threlte/core';
+	import * as Three from 'three';
+	import * as Utils from 'three/src/math/MathUtils';
+</script>
+
+<Threlte.Canvas>
+	<Threlte.PerspectiveCamera position={{ x: 20, y: 20, z: 20 }} fov={50}>
+		<Threlte.OrbitControls />
+	</Threlte.PerspectiveCamera>
+
+	<Threlte.AmbientLight color="white" />
+
+	<Threlte.Mesh
+		geometry={new Three.SphereGeometry(4, 64, 64)}
+		material={new Three.MeshStandardMaterial({ color: 'white' })}
+	/>
+</Threlte.Canvas>
+
 <div class="scene">
 	<slot />
 </div>
