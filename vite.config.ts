712108c9b9ca087c59ba18dbd88ca72860578b5b
GrungeElFz <elf0114_@hotmail.com>
2023-07-02 13:09:29 -0500

    Modified:   Prevent `three` and `troika-three-text` from being externalized for SSR by Vite (vite.config.ts)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const config: UserConfig = {
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['three', 'troika-three-text']
+	}
+};
+
+export default config;
